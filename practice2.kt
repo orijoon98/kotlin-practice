4b004ec7db85c132a365fac822dc36e82cc1601f
HyeokJoon Kong <orijoon98@gmail.com>
2023-08-16 22:51:59 +0900

    add: 변수와 출력

---
+++ practice2.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val name: String = "공혁준"
+    val age = 26
+    println(name)
+    println(age)
+}
---
+++ practice3.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val name: String = "홍길동"
+    val age = 26
+    println("나는 ${name}이고 나이는 ${age}살이야")
+}
