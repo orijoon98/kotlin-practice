c87ea2aec7becf31dccc49afa582068684f5d387
HyeokJoon Kong <orijoon98@gmail.com>
2023-08-16 22:52:27 +0900

    add: 조건문 if 와 when

---
+++ practice4.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val name = "홍길동"
+
+    if (name.equals("홍길동")) {
+        println("내가 나왔네")
+    } else if (name.equals("다른 사람")) {
+        println("다른 사람이네")
+    } else {
+        println("누구세요")
+    }
+}
---
+++ practice5.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name = "홍길동"
+
+    when (name) {
+        "홍길동" -> println("내가 나왔네")
+        "다른 사람" -> println("다른 사람이네")
+        else -> println("누구세요")
+    }
+}
